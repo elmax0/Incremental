b486bb1aade37aed428e8fc9e82bc9176dfdaec1
Maximilian Biwersi <maximilian.biwersi@dvs.ag>
2026-03-16 15:02:38 +0100

    egal

--- src/factory.py
+++ src/factory.py
@@ -9,3 +9,8 @@ class Factory:
         self._initial_upgrade_cost = upgrade_cost
         self._active = active
         pass
+
+    def tick(self, timedelta: float) -> float:
+        if not self._active:
+            return 0.0
+        return self._cash_per_second * timedelta
