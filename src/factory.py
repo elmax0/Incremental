08ce7a6dd57710ef63b2deb8f1f8b05ee0103ed7
Maximilian Biwersi <maximilian.biwersi@dvs.ag>
2026-03-16 15:08:22 +0100

    egal

--- src/factory.py
+++ src/factory.py
@@ -1,16 +1,31 @@
 from decimal import decimal 
 
 class Factory:
-    def __init__(self, name, cash, cash_per_second, upgrade_cost, active: bool = False):
+    def __init__(self, name, health, cash, cash_per_second, upgrade_cost, level: int = 1, active: bool = False):
         self._name = name
+        self._max_health = health
+        self._health = health
         self._cash = cash
         self._cash_per_second = cash_per_second
         self._upgrade_cost = upgrade_cost
         self._initial_upgrade_cost = upgrade_cost
         self._active = active
+        self._level = level
+        self._running = False
         pass
 
     def tick(self, timedelta: float) -> float:
-        if not self._active:
+        if not self._running:
             return 0.0
         return self._cash_per_second * timedelta
+    
+    def click(self, damage) -> float:
+         self._health -= damage
+         if self._heatlh <= 0.0:
+             self._health = self._max_health
+             return self._cash
+         return 0.0
+    
+    def upgrade(self):
+
+        pass
